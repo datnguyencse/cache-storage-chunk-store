36dddbe44324eae07a36303579b27721586afd67
Dat Nguyen <datnguyen@vncdevs.io>
2020-12-30 14:44:29 +0700

    Update cache storage

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ class Storage {
     this.closed = false
     this.length = Number(opts.length) || Infinity
     this.name = opts.name || 'CacheStorageChunkStore'
-
     if (this.length !== Infinity) {
       this.lastChunkLength = this.length % this.chunkLength || this.chunkLength
       this.lastChunkIndex = Math.ceil(this.length / this.chunkLength) - 1
     }
+  }
+
+  async init () {
+    if (this.cache) return this.cache;
+    this.cache = await window.caches.open(this.name);
+    return this.cache;
   }
 
   put (index, buf, cb = noop) {
@@ -33,22 +38,16 @@ class Storage {
       return nextTick(cb, new Error('Chunk length must be ' + this.chunkLength))
     }
 
-    const stream = new window.ReadableStream({
-      start (controller) {
-        controller.enqueue(buf)
-        controller.close()
-      }
-    })
-
-    const response = new window.Response(stream, {
+    const options = {
       status: 200,
       headers: {
         'Content-Type': 'application/octet-stream',
         'Content-Length': buf.length
       }
-    })
+    }
 
-    window.caches.open(this.name).then((cache) => {
+    const response = new window.Response(buf, options)
+    this.init().then((cache) => {
       cache
         .put('/index/' + index, response)
         .then(() => cb(null))
@@ -63,35 +62,16 @@ class Storage {
 
     if (this.closed) return nextTick(cb, new Error('Storage is closed'))
 
-    window.caches.open(this.name).then((cache) => {
+    this.init().then((cache) => {
       cache.match('/index/' + index).then((response) => {
         if (!response) {
-          const err = new Error('Chunk not found')
-          err.notFound = true
-          return cb(err)
+          return cb(new Error('Chunk not found'))
         }
 
-        const isLastChunk = index === this.lastChunkIndex
-        const bytes = new Uint8Array(isLastChunk ? this.lastChunkLength : this.chunkLength)
-
-        const reader = response.body.getReader()
-
-        let offset = 0
-        reader.read().then(function readChunk ({ done, value }) {
-          if (done) {
-            const buf = Buffer.from(bytes)
-            if (!opts) return cb(null, buf)
-            const offset = opts.offset || 0
-            const len = opts.length || buf.length - offset
-            return cb(null, buf.slice(offset, len + offset))
-          }
-
-          bytes.set(value, offset)
-          offset += value.length
-
-          return reader.read().then(readChunk)
+        response.arrayBuffer().then(data => {
+          return cb(null, Buffer.from(data))
         }).catch(cb)
-      })
+      }).catch(cb)
     })
   }
 
@@ -108,7 +88,7 @@ class Storage {
 
     this.closed = true
 
-    window.caches.open(this.name).then((cache) => {
+    this.init().then((cache) => {
       cache.keys().then((keys) => {
         keys.forEach((request) => {
           cache.delete(request)
